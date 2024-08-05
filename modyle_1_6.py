b1d79bffe1ce4e88928ebe5596cd9677184aaa54
AlinaHan1 <alinakoshenko@gmail.com>
2024-08-05 12:15:03 +0300

    Add files via upload

---
+++ modyle_1_6.py
@@ -0,0 +1,22 @@
+#СЛОВАРЬ
+my_dict = {'Alina': 1989,
+           'Olga': 1978,
+           'lera': 1978,
+           'Dania': 1993}
+print(my_dict)
+print(my_dict['Olga'])
+my_dict['Anastasia'] = 1999
+print(my_dict['Anastasia'])
+my_dict.update({'Max': 1999,
+                'Andrey': 1956})
+a = my_dict.pop('Alina')
+print(a)
+print(my_dict)
+# МНОЖЕСТВА
+my_set = {89, 99, 89, 78, 99, 'Alina', 'Anna', 'Alina', (1, 4, 8, 12)}
+print(my_set)
+print(my_set.update({18,'Max'}))
+print(my_set)
+print(my_set.remove(99))
+print(my_set)
+
